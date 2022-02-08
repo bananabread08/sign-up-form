f25fdab207d6874f4276a436970bccf581b96bcb
bananabread08 <rmkfermin@gmail.com>
2022-02-08 10:18:50 +0800

    added fixed form validation for success and fail.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const password = document.getElementById('password');
 const passCheck = document.getElementById('password-check');
 const error = document.querySelector('.error');
-const form = document.querySelector('.my-form');
+const form = document.querySelector('#my-form');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (password.value !== passCheck.value) {
-    alert('no match');
     error.style.display = 'block';
     return;
   }
+  error.style.display = 'none';
+  alert('Account created');
 });
